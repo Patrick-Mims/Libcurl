9f7774791db579e5bc437ac6d07e415c83aed959
Patrick <patrickkevinmims@gmail.com>
2023-03-08 11:14:10 -0800

    latest

--- multi-download/src/threadImpl.c
+++ multi-download/src/threadImpl.c
@@ -24,45 +24,6 @@ struct URL
   int num;
 };
 
-/*
-   void *thread_io(void *arg)
-   {
-   struct email_list *head = (struct email_list *)arg;
-
-   if ((head = malloc(sizeof(struct email_list *))) == NULL)
-   {
-   exit(EXIT_FAILURE);
-   }
-
-   const char *filename = "email-list.csv";
-
-   FILE *in_file = NULL;
-   struct stat sb;
-
-   if ((in_file = fopen(filename, "r")) == NULL)
-   {
-   printf("Error: Opening File\n");
-
-   exit(EXIT_FAILURE);
-   }
-
-   stat(filename, &sb);
-
-   char *file_contents = malloc(sb.st_size);
-
-   while (fscanf(in_file, "%[^\n] ", file_contents) != EOF)
-   {
-   add(&head, file_contents);
-   printf("> %s\n", file_contents);
-   }
-
-   fclose(in_file);
-   exit(EXIT_SUCCESS);
-
-   return NULL;
-   }
-   */
-
 void *thread_curl(void *arg)
 {
   struct URL *url = (struct URL *)arg;
@@ -99,9 +60,16 @@ void *thread_curl(void *arg)
   return NULL;
 }
 
+
+/* size_t
+ * is a type which is used to represent the size of objects in
+ * bytes and is therefore used as the return type by the sizeof operator.
+ * */
 size_t write_file(void *buffer, size_t size, size_t nmemb, void *data)
 {
   struct URL *ft = (struct URL *)data;
+
+  printf("Inside of write_file\n");
 
   if((ft->data = fopen(ft->file, "wb")) == NULL)
   {
@@ -111,26 +79,46 @@ size_t write_file(void *buffer, size_t size, size_t nmemb, void *data)
   return fwrite(buffer, size, nmemb, ft->data);
 }
 
+
+
 void *thread_sftp(void *arg)
 {
+  struct Node {
+    char *fileName;
+  };
+
+  struct Node *ftpNode = NULL;
+
+  if((ftpNode = malloc(sizeof(struct Node))) == NULL)
+  {
+    printf("Cannot Allocate Memory");
+    exit(EXIT_FAILURE);
+  }
+
   printf("Inside thread_sftp\n");
 
   struct URL *ftp = (struct URL *)arg;
 
+  curl_global_init(CURL_GLOBAL_DEFAULT);
   ftp->curl = curl_easy_init();
 
-  curl_easy_setopt(ftp, CURLOPT_URL, "sftp://patrick@192.168.1.167/home/patrick/test.txt");
-  curl_easy_setopt(ftp, CURLOPT_WRITEFUNCTION, write_file);
-  curl_easy_setopt(ftp, CURLOPT_WRITEDATA, &ftp);
-  curl_easy_setopt(ftp, CURLOPT_SSH_AUTH_TYPES, CURLSSH_AUTH_AGENT);
-  curl_easy_setopt(ftp, CURLOPT_VERBOSE, 1L);
+  if(ftp->curl) {
+    printf("ftp->curl is true...\n");
+    curl_easy_setopt(ftp, CURLOPT_URL, "sftp://192.168.1.167/home/patrick/test.txt");
+    curl_easy_setopt(ftp, CURLOPT_WRITEFUNCTION, write_file);
+    curl_easy_setopt(ftp, CURLOPT_WRITEDATA, &ftpNode);
+    curl_easy_setopt(ftp, CURLOPT_SSH_AUTH_TYPES, CURLSSH_AUTH_AGENT);
+    curl_easy_setopt(ftp, CURLOPT_VERBOSE, 1L);
 
-  ftp->response = curl_easy_perform(ftp);
+    ftp->response = curl_easy_perform(ftp);
 
-  curl_easy_cleanup(ftp->curl);
+    curl_easy_cleanup(ftp->curl);
 
-  if(CURLE_OK != ftp->response)
-    fprintf(stderr, "something...%d\n", ftp->response);
+    if(CURLE_OK != ftp->response)
+    {
+      fprintf(stderr, "Error: %d\n", ftp->response);
+    }
+  }
 
   if(ftp->data)
     fclose(ftp->data);
